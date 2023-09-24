dbead92a3c0868b8dcb910a42ebd083cc508c4ca
Gabrielvd <gabrieldeoliveira4@gmail.com>
2023-09-23 21:57:42 -0300

    criação da tela de perfil

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router"
 import Register from '../views/Register.vue'
 import Login from '../views/Login.vue'
 import Principal from '../views/Principal.vue'
+import Perfil from '../views/Perfil.vue'
 
 const routes = [
   { 
@@ -13,6 +14,11 @@ const routes = [
     path: "/login",
     name: 'login',
     component: Login
+  },
+  {
+    path: "/perfil",
+    name: 'perfil',
+    component: Perfil
   },
   {
     path: "/",
---
+++ src/views/Perfil.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="main">
+      <ul class="nav justify-content-end nav-bar ">
+          <li class="nav-item ">
+              <div class="">
+                  <a 
+                  class="nav-link active me-4 link-perfil" 
+                  aria-current="page" 
+                  href="#">
+                      <i class="bi bi-person"></i> Perfil
+                  </a>
+              </div>
+          </li>
+      </ul>
+      <div class="d-flex">      
+        <div class="card row col-md-12 col-10 ">
+              <div class="perfil row">
+
+                <div class="col-2">
+                  <i class="bi bi-person-circle"></i>
+                </div>
+
+                <div class="dados col">
+
+                  <div class="col-md-10 col-6">
+                    <label for="title" class="font-text" 
+                    style="color: black; font-family: 'Red Hat Display', sans-serif;">
+                    Nome de Usuário:
+                    </label>
+                    <input  
+                    v-model="nome" 
+                    type="text" 
+                    class="form-control w-65">
+                  </div>
+
+                  <div class="col-md-10 col-6">
+                    <label for="title" class="font-text" 
+                    style="color: black; font-family: 'Red Hat Display', sans-serif;">
+                    Email educacional:
+                    </label>
+                    <input  
+                    v-model="email" 
+                    type="text" 
+                    class="form-control w-65">
+                  </div>
+                </div>     
+              </div>
+
+              <div class="row">
+
+                <div class="publi">
+                  <label for="title" class="font-text" 
+                  style="color: black; font-family: 'Red Hat Display', sans-serif;">
+                  Publicações feitas:
+                  </label>
+                </div>
+
+                <div class="coment">
+                  <label for="title" class="font-text" 
+                  style="color: black; font-family: 'Red Hat Display', sans-serif;">
+                  Comentarios feitos:
+                  </label>
+                </div>
+              </div>
+
+            </div>
+      </div>
+  </div>
+</template>
+<script setup>
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&family=Red+Hat+Display:wght@600&display=swap');
+@import "bootstrap-icons/font/bootstrap-icons.css";
+.main {
+  background-color: #003049;
+  background-size: cover;
+  margin: 0px;
+}
+.card {
+  background-color: #003049;
+  border-radius: 0px 0px 5px 5px;
+  height: 96vh;
+}
+.nav-bar {
+  background-color: #072231;
+  height: 4vh;
+}
+.bi-person-circle{
+  font-size: 70px;
+}
+
+.link-perfil{
+  display: flex;
+  color: bisque;
+
+}
+
+.perfil.row{
+  background-color: #F8E2CF;
+  border-radius: 0px 0px 5px 5px;
+  max-width: 650px;
+  max-height: 550px;
+  padding: 24px;
+  margin-top: 44px;
+  margin-left: 44px;
+}
+
+.publi{
+  background-color: #F8E2CF;
+  border-radius: 0px 0px 5px 5px;
+  width: 650px;
+  height: 440px;
+  margin-top: 44px;
+  margin-left: 44px;
+}
+.coment{
+  background-color: #F8E2CF;
+  border-radius: 0px 0px 5px 5px;
+  width: 650px;
+  height: 440px;
+  margin-top: 44px;
+  margin-left: 44px;
+}
+.dados {
+  margin-left: 44px;
+}
+
+.create-pub{
+  background-color: #303030;
+  border-radius: 0px 0px 5px 5px;
+  margin-top: -0.1rem !important;
+  width: 95%;
+  margin-left: 2.5%;
+}
+
+.file-imagens{
+  display: flex;
+  position: relative;
+  border: 2px none #e2e2e2;
+  transition: 0.3s;
+  margin-top: 24px;
+  padding: 0;
+  border-radius: 5px;
+  background-color: var(--bs-body-bg);
+  width: 486px;
+  min-height: 300px;
+  justify-content: center;
+  
+}
+
+.btn-custom {
+background-color: #B0B0B0; 
+color: #000000; 
+border-radius: 15px;
+font-family: 'Red Hat Display', sans-serif;
+}
+
+.btn-custom:hover {
+background-color: #303030; 
+color: #ffffff; 
+border-radius: 15px;
+border: 1px none #e2e2e2;
+}
+
+.text-file{
+  margin-top: 4px;
+  font-size: 13px;
+  font-family: 'Red Hat Display', sans-serif;
+  color: #B0B0B0;
+}
+
+</style>
