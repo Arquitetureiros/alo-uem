34cb39b95b4bd51d0e6cc7382d5071a753589dd2
DaniloArr <danilo.arreb@gmail.com>
2023-10-15 22:04:12 -0300

    31-Tela_Principal: fazendo ligacao com endpoints

--- src/utils/Funcoes.js
+++ src/utils/Funcoes.js
@@ -4,3 +4,21 @@ export const campoVazio = (campo) => {
     } 
     return false
 }
+
+export const statusPublicacao = (status) => {
+    switch (status) {
+        case 1:
+            return "Desativado"
+        case 2:
+            return "Resolvido"
+        case 3:
+            return "Em Revisao"
+        case 4:
+            return "Aprovado"
+        case 5:
+            return "Reprovado"
+        case 6: 
+            return "Alteração"
+
+    }
+}
